864f55d3284ddd00e1301645e68af1c843deb980
Fornari-Simone <20simone02@gmail.com>
2024-02-10 16:30:28 +0100

    NuGetAdj

---
+++ LevelingCalculator.API/Migrations/20240210102327_SecondMigration.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LevelingCalculator.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Craftable",
+                table: "Resource",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Own",
+                table: "Resource",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Stars",
+                table: "Character",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "TransactionalOutboxes",
+                columns: table => new
+                {
+                    ID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Tabella = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Messaggio = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransactionalOutboxes", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TransactionalOutboxes");
+
+            migrationBuilder.DropColumn(
+                name: "Craftable",
+                table: "Resource");
+
+            migrationBuilder.DropColumn(
+                name: "Own",
+                table: "Resource");
+
+            migrationBuilder.DropColumn(
+                name: "Stars",
+                table: "Character");
+        }
+    }
+}
---
+++ LevelingCalculator.Repository/Model/TransactionalOutbox.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelingCalculator.Repository.Model
+{
+    public class TransactionalOutbox
+    {
+        public long ID { get; set; }
+        public string Tabella { get; set; } = string.Empty;
+        public string Messaggio { get; set; } = string.Empty;
+    }
+}
